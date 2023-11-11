0e274728ee71827cd52faa0edca09e3efb61204b
Viet Anh Duong Nguyen <98511848+vanhsusu03@users.noreply.github.com>
2023-11-12 01:27:59 +0700

    demo using tailwindcss

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <div class="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
-    Hello World
-  </div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Login></Login>
+  
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" /> -->
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import "./assets/general_css/general.css"
+import "./assets/general_css/style.css";
+import Login from './pages/Login.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Login
+}
 }
 </script>
 
